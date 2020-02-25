bd067e7c2230dbea3c75c04d4ab5c5b35e578a58
VinnyVinVince <thevinnycast@gmail.com>
2020-02-25 09:47:08 -0800

    initial classes

---
+++ src/AnagramSolver.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class AnagramSolver {
+
+    private List<String> dictionary;
+
+    private List<String> answers;
+
+    public AnagramSolver(List<String> list) {
+
+        this.dictionary = list;
+        this.answers = new ArrayList<>();
+
+    }
+
+    public void print(String s, int max) {
+
+        //
+
+    }
+
+}
