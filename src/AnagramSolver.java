eb4843f13b1cf30669359e7a47a081d6f5c8ad86
VinnyVinVince <thevinnycast@gmail.com>
2020-02-28 09:18:07 -0800

    assets & classes

--- src/AnagramSolver.java
+++ src/AnagramSolver.java
@@ -15,7 +15,41 @@ public class AnagramSolver {
 
     public void print(String s, int max) {
 
-        //
+        if (max < 0) {
+
+            throw new IllegalArgumentException();
+
+        }
+
+        explore(s, max);
+
+    }
+
+    private void explore(String s, int num) {
+
+        LetterInventory current = new LetterInventory(s);
+
+        if (current.isEmpty()) {
+
+            System.out.println(this.answers);
+
+        } else {
+
+            for (String word : this.dictionary) {
+
+                LetterInventory difference = current.subtract(new LetterInventory(word));
+
+                if (difference != null) {
+
+                    this.answers.add(word);
+                    explore(difference.toString(), num--);
+                    this.answers.remove(word);
+
+                }
+
+            }
+
+        }
 
     }
 
