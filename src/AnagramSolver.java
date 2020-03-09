952b593f82b3a1d6ac6eb6c9bd8b29ec40d68032
VinnyVinVince <thevinnycast@gmail.com>
2020-03-09 14:22:15 -0700

    finalization

--- src/AnagramSolver.java
+++ src/AnagramSolver.java
@@ -9,17 +9,14 @@ import java.util.*;
  */
 public class AnagramSolver {
 
-    // List of all possible words in an anagram combination.
-    private List<String> dictionary;
+    // All possible words in an anagram combination, each mapped to associated letter counts.
+    private Map<String, LetterInventory> dictionary;
 
     // Temporary storage for possible word combinations.
     private Stack<String> answers;
 
     // Shortened dictionary specific to a phrase for faster anagram generation.
     private Set<String> prunedDict;
-
-    // A mapping of words to associated letter counts for faster operations.
-    private Map<String, LetterInventory> words;
 
     /*
      *  Pre:  List is assumed to be non-empty and contains no duplicates as per project specifications.
@@ -28,14 +25,13 @@ public class AnagramSolver {
      */
     public AnagramSolver(List<String> list) {
 
-        this.dictionary = list;
         this.answers = new Stack<>();
         this.prunedDict = new TreeSet<>();
-        this.words = new HashMap<>();
+        this.dictionary = new HashMap<>();
 
-        for (String word : this.dictionary) {
+        for (String word : list) {
 
-            this.words.put(word.toLowerCase(), new LetterInventory(word));
+            this.dictionary.put(word, new LetterInventory(word));
 
         }
 
@@ -58,9 +54,9 @@ public class AnagramSolver {
         LetterInventory input = new LetterInventory(s);
 
         // Pruning dictionary and populating temporary short dictionary.
-        for (String word : this.dictionary) {
+        for (String word : this.dictionary.keySet()) {
 
-            if (input.subtract(this.words.get(word)) != null) {
+            if (input.subtract(this.dictionary.get(word)) != null) {
 
                 this.prunedDict.add(word);
 
@@ -68,7 +64,7 @@ public class AnagramSolver {
 
         }
 
-        explore(s.toLowerCase(), max);
+        explore(input, max);
 
         // Resetting temporary dictionary after method execution.
         this.prunedDict.clear();
@@ -76,22 +72,9 @@ public class AnagramSolver {
     }
 
     // Private pair with print method. Recursively backtracks to find valid solutions for given phrase and maximum.
-    private void explore(String s, int max) {
+    private void explore(LetterInventory input, int max) {
 
-        LetterInventory current;
-
-        if (this.words.containsKey(s)) {
-
-            // If passed-in phrase is present in dictionary as to not create duplicate letter counts.
-            current = this.words.get(s);
-
-        } else {
-
-            current = new LetterInventory(s);
-
-        }
-
-        if (current.isEmpty() && (max == 0 || this.answers.size() == max)) {
+        if (input.isEmpty() && (max == 0 || this.answers.size() == max)) {
 
             // To exclude answer groupings with less words than the target max, the grouping size must be
             // equal to the target size (excluding the 0-case) to be printed.
@@ -101,7 +84,7 @@ public class AnagramSolver {
 
             for (String word : this.prunedDict) {
 
-                LetterInventory difference = current.subtract(this.words.get(word));
+                LetterInventory difference = input.subtract(this.dictionary.get(word));
 
                 if (difference != null) {
 
@@ -111,7 +94,7 @@ public class AnagramSolver {
                     // it will not be getting closer to the base case, so it will be skipped over.
                     if (max == 0 || this.answers.size() <= max) {
 
-                        explore(difference.toString(), max);
+                        explore(difference, max);
 
                     }
 
